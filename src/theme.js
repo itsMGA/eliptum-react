7c15449dcedd80678b43501ef950aff392340ccb
itsMGA <munteanugabriel95@gmail.com>
2024-01-07 01:03:27 +0200

    declared tmp var

--- src/theme.js
+++ src/theme.js
@@ -4,18 +4,16 @@ const themeMap = {
   solar: "dark"
 };
 
-const theme = localStorage.getItem('theme')
-  || (tmp = Object.keys(themeMap)[0],
-      localStorage.setItem('theme', tmp),
-      tmp);
+let tmp; // Declare tmp variable here
+
+const theme = localStorage.getItem('theme') || (tmp = Object.keys(themeMap)[0], localStorage.setItem('theme', tmp), tmp);
 const bodyClass = document.body.classList;
 bodyClass.add(theme);
 
-export function toggleTheme() {
+function toggleTheme() {
   const current = localStorage.getItem('theme');
   const next = themeMap[current];
 
   bodyClass.replace(current, next);
   localStorage.setItem('theme', next);
 }
-
