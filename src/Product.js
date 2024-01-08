e877974b4d9d41c02d8fc9d21e51b59c9eafa962
itsMGA <munteanugabriel95@gmail.com>
2024-01-08 19:18:32 +0200

    Commented img tag from product comp

--- src/Product.js
+++ src/Product.js
@@ -4,7 +4,7 @@ export default function Product(props) {
   return (
     <div className="card">
       {props.svg}
-      <img className="product--image" src={props.url} alt="product image" />
+      {/* <img className="product--image" src={props.url} alt="product image" /> */}
       <h2>{props.name}</h2>
       <p className="price">{props.price}</p>
       <p>{props.description}</p>
