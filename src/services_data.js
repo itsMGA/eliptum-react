2085260c400f3a393937730d4520eb705c73a7f4
Munteanu Gabriel <92986496+itsMGA@users.noreply.github.com>
2024-01-09 19:49:08 +0000

    Update backend test description and update integration test description

--- src/services_data.js
+++ src/services_data.js
@@ -4,20 +4,20 @@ export const services_data = [
     name: "Frontend Automation",
     className: "frontend",
     price: "$19.99",
-    desc: "Automate your manual frontend tests to gain five key advantages: increased test coverage, faster execution, consistent results, efficient regression testing, and improved overall software quality.",
+    desc: "Automate your manual frontend tests to gain five key advantages: increased test coverage, faster execution, consistent results, efficient regression testing, and improved overall software quality.                               ",
   },
   {
     imageName: "fa-solid fa-microchip",
     name: "Backend Automation",
     className: "backend",
     price: "$19.99",
-    desc: "Automating backend tests ensures efficiency, reliability, comprehensive coverage, streamlined regression testing, and robust functionality, guaranteeing error-free backend operations for efficient software performance.",
+    desc: "Automating backend tests ensures efficiency, reliability, comprehensive coverage, streamlined regression testing, and robust functionality, guaranteeing error-free backend operations.",
   },
   {
     imageName: "fa-solid fa-sitemap",
     name: "Integration Automation",
     className: "integration",
     price: "$19.99",
-    desc: "Automating integration tests ensures efficiency, reliability, comprehensive coverage, streamlined regression testing, and seamless component interaction, ensuring robust, error-free integrations for efficient software performance.",
+    desc: "Integration tests ensure correct collaboration between system components, validating interfaces, unified functionality within applications, enhancing reliability and system cohesion.",
   },
 ];
