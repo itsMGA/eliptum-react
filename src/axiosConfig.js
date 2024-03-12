075dfe67bf61f42a7d8fad0f607054a06399a510
munteanu <munteanu.gabriel@aura.com>
2024-03-12 21:31:55 +0200

    base url log

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,5 +4,5 @@ import axios from "axios";
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_HOST || "http://127.0.0.1:8000/", // Default host
 });
-
+console.log("base url" + baseURL)
 export default axiosInstance;
